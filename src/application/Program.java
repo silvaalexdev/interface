d12f9896c17f3d45b71dec574e436571607fbf78
Alex Silva <silvaalex.dev@gmail.com>
2023-01-19 22:12:29 -0300

    Implementação de entidades

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,30 @@
 package application;
 
+import entities.CarRental;
+import entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
+        System.out.println("Entre com os dados do aluguel: ");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.print("Retirada (DD/MM/YYYY HH:MM): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Retorno (DD/MM/YYYY HH:MM): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        sc.close();
     }
 }
--- src/entities/Vehicle.java
+++ src/entities/Vehicle.java
@@ -2,4 +2,17 @@ package entities;
 
 public class Vehicle {
 
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    private String model;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
